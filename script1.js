d092c4ef1065f30aefb16cfcff237af0572f3f7a
U K Thanish <thanishuk2@gmail.com>
2026-08-06 16:52:11 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,8 @@
+var name1;
+console.log("Hello, " + name1 + "!");
+name1 = "Alice";
+console.log("Hello, " + name1 + "!");
+name1 = "Bob";
+console.log("Hello, " + name1 + "!");
+name1 = "Batman";
+console.log("Hello, " + name1 + "!");
