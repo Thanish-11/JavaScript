9cab1ed1b787409b3364a04092386fec6f7d0826
U K Thanish <thanishuk2@gmail.com>
2026-08-11 17:16:56 +0530

    Add files via upload

---
+++ class2/script.js
@@ -0,0 +1,42 @@
+// 1. Check positive or not
+let num = 10;
+console.log(num > 0 ? "Positive" : "Not Positive");
+
+// 2. Check even or odd
+num = 7;
+console.log(num % 2 === 0 ? "Even" : "Odd");
+
+// 3. Check voting eligibility
+let age = 20;
+console.log(age >= 18 ? "Eligible to vote" : "Not eligible to vote");
+
+// 4. Find greater number
+let a = 25;
+let b = 15;
+console.log(a > b ? a : b);
+
+// 5. Check pass or fail
+let marks = 65;
+console.log(marks >= 35 ? "Pass" : "Fail");
+
+// 6. Check adult or minor
+age = 16;
+console.log(age >= 18 ? "Adult" : "Minor");
+
+// 7. Check login status
+let isLoggedIn = true;
+console.log(isLoggedIn ? "Welcome!" : "Please login");
+
+// 8. Check temperature
+let temperature = 35;
+console.log(temperature > 30 ? "Hot" : "Normal");
+
+// 9. Apply discount
+let price = 1200;
+let finalPrice = price >= 1000 ? price - 100 : price;
+console.log(finalPrice);
+
+// 10. Find smallest number
+a = 10;
+b = 20;
+console.log(a < b ? a : b);
