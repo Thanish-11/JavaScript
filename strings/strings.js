980002c0ab6a8ce3ba94990419c7d81063c23240
U K Thanish <thanishuk2@gmail.com>
2026-08-13 17:16:34 +0530

    Add files via upload

---
+++ strings/strings.js
@@ -0,0 +1,82 @@
+// let name="Thanish"
+// console.log(name);
+// console.log(typeof (name));
+
+// let name2='Hello'
+// console.log(name);
+
+// let name3=`heyy`
+// console.log(name3);
+
+// let age=22;
+// console.log(`My name is ${name} , and my age is ${ age}`);
+
+// let na=new String("GOOD MORNING")
+// console.log(na);
+// console.log(typeof na);
+// console.log(na.length);
+// console.log(na[0]);
+// console.log(na[na.length-1]);
+// //&fromCharCode
+// let output=String.fromCharCode(72,69,76,76,79)
+// console.log(output);
+// let java=String.fromCharCode(74,97,118,97,83,99,114,105,112,116)
+// console.log(java);
+// console.log(String.fromCharCode(128583));
+// console.log(String.fromCodePoint(128583));
+// let str=String.raw`hello \n world ${8+9}`
+// console.log(str);
+
+// let data="Java Script"
+// console.log(data.at(0));
+// //&charAt &charCodeAt
+// console.log(data.charAt(6));
+// console.log(data.charAt(-6));
+// console.log(data.charCodeAt(1));
+
+// //&concat
+// let d="Good"
+// let d1="Morning"
+// let d3="Thanish"
+// let d4=d.concat(" ",d1," ",d3);
+// console.log(d4);
+
+// console.log(d4.includes("Good"));
+// console.log(d4.includes("Go"));
+// console.log(d4.includes("M"));
+
+// //&indexOf()
+// console.log(d4.indexOf("o"));
+
+// //&lastIndexOf
+// console.log(d4.lastIndexOf("o"));
+
+// //*split()
+// console.log(d4.split());
+// console.log(d4.split(""));
+// console.log(d4.split(" "));
+// console.log(d4.split("o"));
+// console.log(d4.toUpperCase());
+// console.log(d4.toLowerCase());
+// //^trim()
+// let Data = "React"
+// console.log(Data.length);
+// let result=Data.trim();
+// console.log(Data.trim().length);
+// console.log(Data.trimStart());
+// console.log(Data.trimEnd());
+// console.log(Data.startsWith(" Rea"));
+// console.log(Data.endsWith(" ct",5));
+// console.log(Data.repeat(5));
+// console.log(Data.padEnd(7,"JS"));
+// let r=Data.padEnd(7,"JS")
+// console.log(r.replace("JS","YE"));
+// console.log(d4.replaceAll("a","L"));
+// console.log(d4.replaceAll("o","L"));
+// let obj=new String("Heyyy")
+// console.log(typeof obj.toString());
+
+let d="WELCOME TO JAVASCRIPT LIFE"
+console.log(d.replaceAll(" ",""));
+console.log(d.slice(3))
+console.log(d.substring(1,25));
